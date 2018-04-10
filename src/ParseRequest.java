36f92a30718cb7c5935ec2577ee712574d4c7553
stevenmata11 <tcmata11@hotmail.com>
2018-04-10 16:34:22 -0500

    Add database

--- src/ParseRequest.java
+++ src/ParseRequest.java
@@ -1,3 +1,4 @@
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -11,10 +12,9 @@ public class ParseRequest {
 
     Database database;
 
-    public ParseRequest(Database database){
+    public ParseRequest(Database database) {
         this.database = database;
     }
-
 
 
     public void parseLogin(JSONObject request, BufferedReader in, PrintWriter out) throws SQLException {
@@ -23,14 +23,14 @@ public class ParseRequest {
 
         ResultSet resultSet = database.runQuery("Select * From AccountInfo");
 
-        while ( resultSet.next() ) {
+        while (resultSet.next()) {
             try {
                 int id = resultSet.getInt("userId");
                 String password = resultSet.getString("hashedPassword");
 
 
-                System.out.println( "UserID = " + id );
-                System.out.println( "Hashed Password = " + password );
+                System.out.println("UserID = " + id);
+                System.out.println("Hashed Password = " + password);
                 System.out.println();
 
 
@@ -42,7 +42,7 @@ public class ParseRequest {
 
     }
 
-    public void parseNewUser(JSONObject request, BufferedReader in, PrintWriter out){
+    public void parseNewUser(JSONObject request, BufferedReader in, PrintWriter out) {
         System.out.println("New User Request");
 
         try {
@@ -57,10 +57,8 @@ public class ParseRequest {
             String userName = request.getString("userName");
 
 
-
-
-             database.runUpdate("INSERT INTO AccountInfo(userID,hashedPassword,cryptoID,cryptoPrivateKey,cryptoPublicKey,balance,email,userName,phoneNumber)" +
-                "VALUES ('" + userId + "','" + hashedPassword + "','" + cryptoID + "','" + cryptoPrivateKey + "','" + cryptoPublicKey + "',' balance ','" + email +"','"userName"','phoneNumber' )");
+            database.runUpdate("INSERT INTO AccountInfo(userID,hashedPassword,cryptoID,cryptoPrivateKey,cryptoPublicKey,balance,email,userName,phoneNumber)" +
+                    "VALUES ('" + userId + "','" + hashedPassword + "','" + cryptoID + "','" + cryptoPrivateKey + "','" + cryptoPublicKey + "',' balance ','" + email + "','"userName"','phoneNumber' )");
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -77,8 +75,57 @@ public class ParseRequest {
         out.println(send.toString());
     }
 
-    public void parseHistory(JSONObject request, BufferedReader in, PrintWriter out){
+    public void parseHistory(JSONObject request, BufferedReader in, PrintWriter out) {
+        //transactionID, sender, receiver, status, amount, senderOrReceiver(receiveristrue)
+        try {
+            String userID = request.getString("UID");
+
+            JSONArray array = new JSONArray();
+            JSONObject object;
+            ResultSet resultSet = database.runQuery("");
+            // Get transaction where (uid = senderId or uid = receiverID) && update = true
+            double amount;
+            while (resultSet.next()) {
+                object = new JSONObject();
+
+                amount = resultSet.getDouble("smount");
+                if (userID.equals(resultSet.getString("senderID")))
+                    amount *= -1;
+
+                //do query to get sender or receiver name
+                ResultSet resultSet2;
+                if (userID.equals(resultSet.getString("senderID"))) {
+                    //get username for receiver id
+                    resultSet2 = database.runQuery("");
+                } else {
+                    //get username for sender id
+                    resultSet2 = database.runQuery("");
+                }
+                //
+                resultSet2.next();
+                String userName = resultSet2.getString("userName");
+
+                object.put("transactionID", resultSet.getString("transactionID"))
+                        .put("to_from", userName)
+                        .put("status", resultSet.getString("status"))
+                        .put("amount", amount);
+
+                array.put(object);
+
+            }
+
+            object = new JSONObject();
+            object.put("array", array);
+
+            out.println(object.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         System.out.println("History Request");
     }
-
 }
