5d49835a90ea0f8c685366d5000bbc1568cb15c3
stevenmata11 <tcmata11@hotmail.com>
2018-04-15 21:34:31 -0500

    Add database

--- src/ParseRequest.java
+++ src/ParseRequest.java
@@ -7,8 +7,6 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.UUID;
-
 import javax.xml.crypto.Data;
 
 
@@ -20,17 +18,11 @@ public class ParseRequest {
         this.database = database;
     }
 
-
     public void parseLogin(JSONObject request, BufferedReader in, PrintWriter out){
 
         System.out.println("Login Request");
 
-        //ResultSet resultSet = database.runQuery("Select * From AccountInfo");
-
-      //  while (resultSet.next()) {
             try {
-           /*     int id = resultSet.getInt("userId");
-                String password = resultSet.getString("hashedPassword");*/
                 String userName = null;
                 try {
                     userName = request.getString("userName");
@@ -44,11 +36,9 @@ public class ParseRequest {
                     e.printStackTrace();
                 }
 
-
                 ResultSet passwordCheck = database.runQuery("SELECT hashedPassword" +
                                 "FROM AccountInfo" +
                                 "WHERE userName = " + userName);
-
                 if(!passwordCheck.next()) {
                     //username wrong
                     JSONObject jsonObject = new JSONObject();
@@ -70,7 +60,6 @@ public class ParseRequest {
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-
                 } else {
                     JSONObject jsonObject = new JSONObject();
                     try {
@@ -80,7 +69,6 @@ public class ParseRequest {
                         e.printStackTrace();
                     }
                 }
-
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -99,8 +87,6 @@ public class ParseRequest {
             //Double balance = request.getString("balance");
             String userName = request.getString("userName");
 
-
-
             ResultSet userCheck = database.runQuery("SELECT userName" +
                             "FROM AccountInfo" +
                             "WHERE userName = " + userName);
@@ -115,8 +101,8 @@ public class ParseRequest {
                 double random = new Random().nextDouble();
                 double result = start + (random * (end - start));
                 double balance = result;
-                //ResultSet resultSet = database.runQuery("INSERT INTO AccountInfo(hashedPassword,cryptoID,cryptoPrivateKey,cryptoPublicKey,balance,email,userName,phoneNumber)" +
-                //        " VALUES (" + hashedPassword + "','" + cryptoID + "','" + cryptoPrivateKey + "','" + cryptoPublicKey + "',' balance ','" + email + "','"userName"','phoneNumber' )" +
+                //ResultSet resultSet = database.runQuery("INSERT INTO AccountInfo(userName, hashedPassword,cryptoID,cryptoPrivateKey,cryptoPublicKey,balance,email,phoneNumber)" +
+                //        " VALUES (" userName + "','" + hashedPassword + "','" + cryptoID + "','" + cryptoPrivateKey + "','" + cryptoPublicKey + "',' balance ','" + email + "',' phoneNumber' )" +
                 //        " SELECT last_insert_rowid()");
                 //resultSet.next();
                 //String id = resultSet.getString("userID");
@@ -143,6 +129,7 @@ public class ParseRequest {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 
     public void parseTransaction(JSONObject request, BufferedReader in, PrintWriter out) throws JSONException {
@@ -159,20 +146,15 @@ public class ParseRequest {
         Double amount = request.getDouble("amount");
         Long time = System.currentTimeMillis();
         String status = "pending";
-        //String
-
-        //int TransactionID = Integer.parseInt(maxTransactionID) + 1;
 
         database.runUpdate("INSERT into Transactions (sender, receiever, amount, time, status)" +
                         "Values( " + sender + "," + receiver + ", " + amount + "," + time + ", " + status + ")");
 
         send.put("Status", "Completed Transaction");
         out.println(send.toString());
-
     }
 
     public void parseHistory(JSONObject request, BufferedReader in, PrintWriter out) {
-        //transactionID, sender, receiver, status, amount, senderOrReceiver(receiveristrue)
         try {
             String username = request.getString("username");
 
@@ -191,7 +173,7 @@ public class ParseRequest {
                                         "WHERE Transactions.receiver  != " + username);
 
 
-            // Get transaction where (uid = senderId or uid = receiverID) && update = true
+
             double amount;
             while (resultSet.next()) {
                 object = new JSONObject();
@@ -215,7 +197,6 @@ public class ParseRequest {
 
 
             System.out.println(object.toString());
-            //out.println(object.toString());
 
         } catch (JSONException e) {
             e.printStackTrace();
